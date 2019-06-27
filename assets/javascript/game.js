c735f274977bc1b410199e30dd68a3d48151c1a6
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-06-27 17:56:31 -0400

    added music player, changed background and array to space theme. working on check to see if user input is a string.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
 var answer = [             // The guessable word array
-    "this",
-    "is",
-    "not",
-    "coolio",
-    "homie"
+    "mercury",
+    "venus",
+    "earth",
+    "mars",
+    "jupiter",
+    "saturn",
+    "uranus",
+    "neptune",
+    "Pluto"
 ];
 
 
@@ -18,7 +22,7 @@ var guessingWord = [];  // The array that holds the correct letters guessed.
 var wins = 0;
 var guessesLeft =0;
 var correct = false;
-var gameEnd = false;   
+
 
 
 
@@ -37,7 +41,6 @@ console.log(guessingWord);
 
 
 
-var guessedLetters = document.getElementById("letters");
 document.onkeyup = function(event) {
           checkLetter(event.key.toUpperCase())
      }
@@ -45,10 +48,13 @@ document.onkeyup = function(event) {
      var lives = document.getElementById("lives").innerText = triesLeft; //Display "10" tries left
      var triesDisplay = lives;
 
-     var word = document.getElementById("guess").innerText = guessingWord.join(" "); //Display The current word being guessed
-     var wordDisplay = word;
+     var correctWord = document.getElementById("guess").innerText = guessingWord.join(" "); //Display The current word being guessed
+     var wordDisplay = correctWord;
 
      var winCount = document.getElementById("wins").innerText = wins; //Display wins
+
+     var guessingLetters = document.getElementById("letters").innerText = lettersGuessed.join(" "); //Display letters guessed
+     var guessedLettersDisplay = guessingLetters;
 
      function checkLetter(letter){
           var correctLetter = false;    //Function Checking if the letter is correct.
@@ -56,14 +62,17 @@ document.onkeyup = function(event) {
     
 
      for (var i=0; i < currentWord.length; i++){
-          if (letter === currentWord[i]){             // if the letter is correct  
-               guessingWord[i] = letter;
-               correctLetter = true;
-          }
-               if(guessingWord.join("") === currentWord){
-                    wins++
-                    reset();
+          if(typeof letter == 'string'){
+          
+               if (letter === currentWord[i]){             // if the letter is correct  
+                    guessingWord[i] = letter;
+                    correctLetter = true;
                }
+                    if(guessingWord.join("") === currentWord){
+                         wins++
+                         reset();
+                    }
+          }
      }
 
      if (!correctLetter){
@@ -73,43 +82,67 @@ document.onkeyup = function(event) {
           }
           if(triesLeft === 0) {
                guessingWord = currentWord.split();  // Display correct word if you run out of tries.
+               restart();
+               alert("Game over. Click to start over");
           }
      }
 
 
-guessedLetters.textContent = (event.key).toUpperCase();  // displays the key pressed
-  var newH = $("<h1>");
-  var x = lettersGuessed.toString();
-  var y = x.toUpperCase();
-  newH.text(y);
-  $("#letters").append(newH);
-
      triesDisplay = document.getElementById("lives").innerText = triesLeft; // Displays remaining tries.
      wordDisplay = document.getElementById("guess").innerText = guessingWord.join(" ");   // Update guessing word _ with correct letter
      winsDisplay = document.getElementById("wins").innerText = wins; // Update wins 
+     guessedLettersDisplay = document.getElementById("letters").innerText = lettersGuessed.join(" "); // display guessed letters
 };
 
 function reset(){
-     currentWord = answer[Math.floor(Math.random() * answer.length)].toUpperCase(); 
-     console.log(currentWord);     //select a random word
-
-     for (var i=0; i < currentWord.length; i++){     // This for loop takes the picked word and replaces it with "_" 
-     guessingWord.push("_");
-     
-}
+     currentWord = answer[Math.floor(Math.random() * answer.length)].toUpperCase(); // select a random word from the array 
+         
 
      triesLeft = tries;  // reset tries left
 
      lettersGuessed = [];     // reset arrays
      guessingWord = [];
 
+     for (var i=0; i < currentWord.length; i++){     // This for loop takes the picked word and replaces it with "_" 
+     guessingWord.push("_"); 
+     }
+     console.log(currentWord);  
+     console.log(guessingWord);  
+
+
      triesDisplay = document.getElementById("lives").innerText = triesLeft; // Displays remaining tries.
      wordDisplay = document.getElementById("guess").innerText = guessingWord.join(" ");   // Update guessing word _ with correct letter
      winsDisplay = document.getElementById("wins").innerText = wins; // Update wins 
-
-
+     guessedLettersDisplay = document.getElementById("letters").innerText = lettersGuessed.join(" ");
 }
 
+function restart(){
+
+     currentWord = answer[Math.floor(Math.random() * answer.length)].toUpperCase(); // select a random word from the array 
+         
+
+     triesLeft = tries;  // reset tries left
+
+     lettersGuessed = [];     // reset arrays
+     guessingWord = [];
+
+     for (var i=0; i < currentWord.length; i++){     // This for loop takes the picked word and replaces it with "_" 
+     guessingWord.push("_"); 
+     }
+     console.log(currentWord);  
+     console.log(guessingWord);  
+
+     wins = 0;      //reset wins to 0
+
+     triesDisplay = document.getElementById("lives").innerText = triesLeft; // Displays remaining tries.
+     wordDisplay = document.getElementById("guess").innerText = guessingWord.join(" ");   // Update guessing word _ with correct letter
+     winsDisplay = document.getElementById("wins").innerText = wins; // Update wins 
+     guessedLettersDisplay = document.getElementById("letters").innerText = lettersGuessed.join(" ");
+}
+
+function letterCheck(ch){
+
+}
 
 
 });
