26d69231fb30127cb78c24161f5aa9f08298314d
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-06-27 14:29:57 -0400

    Added wins counter and lives left counter to page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,28 +20,49 @@ var gameEnd = false;
 
 
 
-currentWord = answer[Math.floor(Math.random() * answer.length)]; // Picks the current word from the array
+currentWord = answer[Math.floor(Math.random() * answer.length)].toUpperCase(); // Picks the current word from the array
     
 for (var i=0; i < currentWord.length; i++){     // This for loop takes the picked word and replaces it with "_" 
     guessingWord.push("_");
 }
 
+console.log(currentWord);
 console.log(guessingWord);
 
-var targetDiv = document.getElementById("guess");
+
 
 for (var d = 0; d < 1; d++){
-     var newP = $("<p>");  
+     var newH = $("<h1>");  
      var x = guessingWord.toString();             
-     newP.text(guessingWord);            
-     $("#guess").append(newP); 
+     newH.text(x);            
+     $("#guess").append(newH); 
 }
+
+
 
 var guessedLetters = document.getElementById("letters");
 document.onkeyup = function(event) {
-  guessedLetters.textContent = event.key;
+guessedLetters.textContent = event.key;  // displays the key pressed
+lettersGuessed.push(event.key);
+  var newH = $("<h1>");
+  var x = lettersGuessed.toString();
+  newH.text(x);
+  $("#letters").append(newH);
 };
 
-// $("#letters").keyup(function(event) {
-//      console.log(event.key);
-//    });
+
+
+var lives = document.getElementById("lives");
+    for(var l = 0; l < 1; l++){
+         var newH = $("<h1>");
+         newH.text("Lives left =" + tries);
+          $("#lives").append(newH);
+    }
+
+var score = document.getElementById("wins");
+    for(var l = 0; l < 1; l++){
+         var newH = $("<h1>");
+         newH.text("Wins = " + wins);
+          $("#wins").append(newH);
+    }
+
